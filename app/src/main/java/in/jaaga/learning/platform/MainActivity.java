94c3094c87334b422d4309576548f3eee98789f5
Quijano Flores <quijanoflores@gmail.com>
2016-11-03 13:04:35 +0530

    fixed the InvalidFormatException

--- app/src/main/java/in/jaaga/learning/platform/MainActivity.java
+++ app/src/main/java/in/jaaga/learning/platform/MainActivity.java
@@ -125,7 +125,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         super.onCreate(savedInstanceState);
 
         Fabric.with(this, new Crashlytics());
-
+        System.setProperty("org.xml.sax.driver","org.xmlpull.v1.sax2.Driver");
         setContentView(R.layout.activity_main);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
